96147508a96242508b41a4a61cd3a53290e289fe
John Pope <jp@fieldstormapp.com>
2021-12-16 22:50:15 +1100

    ok

--- example/components/grid.tsx
+++ example/components/grid.tsx
@@ -1,10 +1,13 @@
 import { postAndWait } from './grid.utils';
-
+import DatePicker from 'react-datepicker';
 import React from 'react';
-import { SupabaseGrid, SupabaseGridRef, SupaRow ,Filter, FilterOperator} from '@supabase/grid';
+import { SupabaseGrid, SupabaseGridRef, SupaRow, Filter, FilterOperator } from '@supabase/grid';
 
 
 export default function Grid() {
+  const [startDate, setStartDate] = React.useState<Date>(new Date()) // Date
+  const [endDate, setEndDate] = React.useState<Date>(new Date()) // Date
+
   const gridRef = React.useRef<SupabaseGridRef>(null);
   const [tableInput, setTableInput] = React.useState('transactions');
   const [schemaInput, setSchemaInput] = React.useState('public');
@@ -19,7 +22,7 @@ export default function Grid() {
   const [reload, setReload] = React.useState(false);
   var testFilters: Filter[] = [];
 
-  
+
 
   function onReadonlyInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     setReadonly(e.target.checked);
@@ -28,14 +31,14 @@ export default function Grid() {
   }
 
   function debugGrid() {
-    console.log("1 gridReg:",gridRef);
+    console.log("1 gridReg:", gridRef);
     if (gridRef.current) {
-      let filters = gridRef.current.getTheFilters();
-      console.log("filters:",filters);
-      console.log("filters[0]:",filters[0]);
-      
-      for (var f in filters){
-        if (f.includes("account_id")){
+      let filters: Filter[] = gridRef.current.getTheFilters();
+      console.log("filters:", filters);
+
+
+      for (var f in filters) {
+        if (filters[0]["column"] == 'account_id') {
           return
         }
       }
@@ -45,7 +48,7 @@ export default function Grid() {
     if (gridRef.current) gridRef.current.addFilter(test[0]);
     // if (gridRef.current) gridRef.current.rowAdded({});
 
-    
+
   }
   function onRowAdded() {
     if (gridRef.current) gridRef.current.rowAdded({});
@@ -76,9 +79,19 @@ export default function Grid() {
     return filters
   }
 
+  function onStartDateChange(date: Date) {
+    setStartDate(date);
+    var test = filter('date', '>', date.toISOString());
+    if (gridRef.current) gridRef.current.addFilter(test[0]);
+  }
 
+  function onEndDateChange(date: Date) {
+    setEndDate(date);
+    var test = filter('date', '<=', date.toISOString());
+    if (gridRef.current) gridRef.current.addFilter(test[0]);
+  }
   React.useEffect(() => {
-    
+
     if (uiMode == 'dark') {
       document.body.classList.add('dark');
     } else {
@@ -89,43 +102,16 @@ export default function Grid() {
   return (
     <div className="main-container">
       <div className="tool-bar">
-        {/* <input
-          value={schemaInput}
-          placeholder="Schema name"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setSchemaInput(e.target.value);
-          }}
-        /> */}
-        <input
-          value={tableInput}
-          placeholder="Table name"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setTableInput(e.target.value);
-          }}
-          style={{ marginLeft: '10px' }}
+        <DatePicker selected={startDate}
+          onChange={(date: Date) => onStartDateChange(date)}
         />
-        <button onClick={reloadGrid} style={{ marginLeft: '10px' }}>
-          Reload Data Grid
-        </button>
-        {/* <label style={{ marginLeft: '10px' }}>
-          readonly:
-          <input
-            name="readonly"
-            type="checkbox"
-            checked={isReadonly}
-            onChange={onReadonlyInputChange}
-          />
-        </label> */}
-         <button onClick={debugGrid} style={{ marginLeft: '1rem' }}>
-         Debug Grid
-        </button> 
-        {/* <button onClick={onToggleDarkMode} style={{ marginLeft: '1rem' }}>
-          Dark Mode Toggle
-        </button> */}
+        <DatePicker selected={endDate}
+          onChange={(date: Date) => onEndDateChange(date)}
+        />
       </div>
       {!reload && (
 
- 
+
         <div className="grid-container">
           <SupabaseGrid
             ref={gridRef}
@@ -133,11 +119,11 @@ export default function Grid() {
             schema={table.schema}
             filters={testFilters}
             editable={!isReadonly}
-            
+
             storageRef="dqofwyqljsmbgrubmnzk"
             theme={uiMode}
             gridProps={{ height: '100%' }}
-           
+
             onError={(error) => {
               console.log('ERROR: ', error);
             }}
@@ -158,24 +144,24 @@ export default function Grid() {
               console.log('edit row: ', row.idx);
             }}
             onSqlQuery={async (query: string) => {
-              console.log('query:',query);
-              
+              console.log('query:', query);
+
               // console.log('SupabaseGrid.propTypes:',SupabaseGrid.propTypes);
               // console.log('SupabaseGrid.props:',this.prop);
-              
+
               // var strQuery = "select count(*) from public.transactions where account_id = 258;";;
-              console.log("gridProps:",self);
+              console.log("gridProps:", self);
               // if (query.includes("select * from public.transactions order by id asc nulls last limit 100 offset 0;")){
               //   query = "select * from public.transactions where account_id = 258 order by id asc nulls last limit 100 offset 0; "
               // }
               const res = await postAndWait('/api/sql-query', { query });
-              console.log("res:",res);
+              console.log("res:", res);
               return res;
             }}
             headerActions={
               <>
-                <span>{`'{headerActions}' can be used to insert`}</span>,
-                {/* <button>SAVE CSV HERE</button>, */}
+                {/* <span>{`'{headerActions}' can be used to insert`}</span>,
+                <button>SAVE CSV HERE</button>, */}
               </>
             }
           />
--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// import '../node_modules/react-calendar/dist/Calendar.css'
+import '../node_modules/react-datepicker/dist/react-datepicker.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
